ebf38999969ef920a071e62f5c9901afb7cc0a42
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-17 21:22:43 +0000

    update demo js website 1

---
+++ week4/demo-js-website-1/js/js-demo-3.js
@@ -0,0 +1,17 @@
+//javascript single line comment 
+
+/*
+javascript multi 
+line comment 
+*/
+
+//group code in a name called function
+//define function 
+function display_info_1()
+{
+    console.log("# Js Demo 2");
+    //alert("# Js Demo 2")
+}
+
+//call function 
+display_info_1();
