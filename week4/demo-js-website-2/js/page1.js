e02e7d35b1393d973fa314202420b0e263be2bc3
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-18 17:54:42 +0000

    update demo js website 2

--- week4/demo-js-website-2/js/page1.js
+++ week4/demo-js-website-2/js/page1.js
@@ -1,5 +1,27 @@
 //javascript single line comment 
 
 /*
-javascript multi line comment 
+javascript multi 
+line comment 
 */
+
+function display_info1(){
+    console.log("# page1 - display info")  
+}
+
+function page_load(){
+  console.log("# page1 - page load")
+  display_info1();
+}
+
+function demo_link1() {
+    console.log("# page1 - link1")
+    alert("# page1 - link1")
+}
+
+function demo_button1() 
+{
+    console.log("# page1 - button1")
+    alert("# page1 - button1")
+}
+
