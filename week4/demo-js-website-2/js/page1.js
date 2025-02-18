40beef2f92748908803513a5b5fd74647e51de58
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-18 19:50:16 +0000

    update demo js website 2

--- week4/demo-js-website-2/js/page1.js
+++ week4/demo-js-website-2/js/page1.js
@@ -76,4 +76,55 @@ function demo_variables1(){
 }
 
 
+function demo_variables2(){
+
+    console.log("# page1 - variables2")
+    //alert("# page1 - variables1")
+
+    //variable - in-memory data, to use later in your code 
+    //data type - type odf data stored in memory - string, number, boolean, true/false, other, other 
+
+    //define memory variable to be used later in code 
+    //global variables - accessed by other function 
+    //global or page level variables - defined outside a function, access by any functoin 
+    //function variables - access only by current function 
+
+    //var - javascript es1-4 - 1997-2008
+    //var - define a local/function in-memory variable 
+    //not including var keyword is same as var 
+    var var1 = "intro to javascrpt";  //data-type - string
+    var var_2 = "6"; //data-type - string
+    var var2 = 6  //data-type - number integer
+    var var3 = 6.1  //data-type - number floating point including decimal point 
+    var var4 = true //data-type - boolean - true/false
+
+    //other data types - boolean = true/false, object
+
+    //can convert string to number 
+
+    //when function end - memory variable is removed from memory
+
+    //use variable 
+    console.log(var1)
+
+    //combine string variables = concatination 
+    console.log(var1 + var_2)
+
+    //combine string and number  = concatination 
+    console.log(var1 + var2)
+
+    //combine string and number  = concatination 
+    console.log(var1 + " " + var2)
+
+    //combine varables  = concatination 
+    console.log(var1,var2,var3)
+
+    //update varible 
+    var3 = 6.3 //data type = float, can update to any other data type 
+
+    //combine varables  = concatination 
+    console.log(var1,var2,var3)
+    
+}
+
 
