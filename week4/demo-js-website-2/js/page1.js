d370d8cac4a7d1ff29c63f07da0eb0027c6893b3
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-18 20:02:55 +0000

    update demo js website 2

--- week4/demo-js-website-2/js/page1.js
+++ week4/demo-js-website-2/js/page1.js
@@ -127,4 +127,111 @@ function demo_variables2(){
     
 }
 
+function demo_variables3(){
+
+    console.log("# page1 - variables3")
+
+    //variable - in-memory data, to use later in your code 
+    //data type - type odf data stored in memory - string, number, boolean, true/false, other, other 
+
+    //define memory variable to be used later in code 
+    //global variables - accessed by other function 
+    //global or page level variables - defined outside a function, access by any functoin 
+    //function variables - access only by current function 
+
+    //let - javascript es5+ - 2009
+    //let - define a local/function in-memory variable  
+    //let - reccomended: more optimized memory variable
+    let var1 = "intro to javascrpt";  //data-type - string
+    let var_2 = "6"; //data-type - string
+    let var2 = 6  //data-type - number integer
+    let var3 = 6.1  //data-type - number floating point including decimal point 
+    let var4 = true //data-type - boolean - true/false
+
+    //other data types - boolean = true/false, object
+
+    //can convert string to number 
+
+    //when function end - memory variable is removed from memory
+
+    //use variable 
+    console.log(var1)
+
+    //combine string variables = concatination 
+    console.log(var1 + var_2)
+
+    //combine string and number  = concatination 
+    console.log(var1 + var2)
+
+    //combine string and number  = concatination 
+    console.log(var1 + " " + var2)
+
+    //combine varables  = concatination 
+    console.log(var1,var2,var3)
+
+    //const - readonly, not to be updated
+    //varibles const - can be updated by not reccomended to update 
+    //update varible 
+    //javascript does not generate an error if const are updated 
+    var3 = 6.3 //data type = float, can update to any other data type 
+
+    //combine varables  = concatination 
+    console.log(var1,var2,var3)
+    
+}
+
+
+function demo_variables4(){
+
+    console.log("# page1 - variables4")
+
+    //variable - in-memory data, to use later in your code 
+    //data type - type odf data stored in memory - string, number, boolean, true/false, other, other 
+
+    //define memory variable to be used later in code 
+    //global variables - accessed by other function 
+    //global or page level variables - defined outside a function, access by any functoin 
+    //function variables - access only by current function 
+
+    //const - javascript es5+ - 2009
+    //const - define a local/function in-memory variable that should not be updated when defined 
+    //const - reccomended: define and not update const variable later in code 
+    //const = fixed and readonly when assined 
+    const var1 = "intro to javascrpt";  //data-type - string
+    const var_2 = "6"; //data-type - string
+    const var2 = 6  //data-type - number integer
+    const var3 = 6.1  //data-type - number floating point including decimal point 
+    const var4 = true //data-type - boolean - true/false
+
+    //other data types - boolean = true/false, object
+
+    //can convert string to number 
+
+    //when function end - memory variable is removed from memory
+
+    //use variable 
+    console.log(var1)
+
+    //combine string variables = concatination 
+    console.log(var1 + var_2)
+
+    //combine string and number  = concatination 
+    console.log(var1 + var2)
+
+    //combine string and number  = concatination 
+    console.log(var1 + " " + var2)
+
+    //combine varables  = concatination 
+    console.log(var1,var2,var3)
+
+    //const - readonly, not to be updated
+    //varibles const - can be updated by not reccomended to update 
+    //update varible 
+    //javascript does not generate an error if const are updated 
+    var3 = 6.3 //data type = float, can update to any other data type 
+
+    //combine varables  = concatination 
+    console.log(var1,var2,var3)
+    
+}
 
