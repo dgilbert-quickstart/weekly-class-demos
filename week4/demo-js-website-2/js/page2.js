9a11c10234089281557f60933b69667c9c65c390
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-19 18:38:11 +0000

    update js demo website 2

--- week4/demo-js-website-2/js/page2.js
+++ week4/demo-js-website-2/js/page2.js
@@ -10,3 +10,16 @@ function page_load(){
     console.log("# page2: page_load");
 }
 
+function lnk_dom_demo_1()
+{
+    //Js DOM- select an html element in the page the manipluate elementie: update element text 
+
+    let msgText = "# Introduction to js DOM"
+
+    //document = global javascript object ie: DOM
+    //getElementById = select html element by  id 
+    document.getElementById("div-display-info").innerText = msgText;
+
+}
+
+
