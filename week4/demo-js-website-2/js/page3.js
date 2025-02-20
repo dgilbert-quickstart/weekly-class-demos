763826c2583af7a811466dfffe819864ef854b74
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-20 22:41:22 +0000

    update js demo website 2, update page3

--- week4/demo-js-website-2/js/page3.js
+++ week4/demo-js-website-2/js/page3.js
@@ -11,7 +11,61 @@ function btnSubmit(){
     const msgText = "# page3: btnSubmit";
 
     console.log(msgText.toUpperCase())
-    
+  
+    /*
+        div-display-info:
+            1- select div-display-info using document.getElementById
+            2- validate if div-display-info is not null or undefined, if so, display element not found message and exit function
+    */
+
+    /*
+        firtname:
+            1- select firstname field using document.getElementById
+            2- validate if firstname field is not null or undefined, if so, display field not found message and exit function
+            3- validate if firstname field is empty, if so, display invalid field entry message and exit function
+    */
+
+    /*
+        lastname:
+            1- select lastname field using document.getElementById
+            2- validate if lastname field is not null or undefined, if so, display invalid field entry message and exit function
+    */
+
+    /*
+        email:
+            1- select email field using document.getElementById
+            2- validate if email field is not null or undefined, if so, display field not found message and exit function
+            3- validate if email field is empty, if so, display invalid field entry message and exit function
+    */
+
+    /*
+        comment:
+            1- select comment field using document.getElementById
+            2- validate if comment field is not null or undefined, if so, display field not found message and exit function
+            3- validate if comment field is empty, if so, display invalid field entry message and exit function
+    */
+
+    /*
+        //- display form field information in log ie: 
+
+            msgText = "form submitted: firstname: " + txtfirstname.value + ", lastname: " + 
+            txtlastname.value + ", email: " + txtemail.value + ", comment: ";
+
+            console.log(msgText)
+    */
+
+    //divdisplayinfo.innerText = "form submitted"
+
+    /*
+        //clear each field value 
+        txtfirstname.value = "";
+        txtlastname.value = "";
+        txtemail.value = "";
+        txtcomment.value = "";
+
+        txtfirstname.focus();
+    */
+
     //prevent form post
     return false
 
