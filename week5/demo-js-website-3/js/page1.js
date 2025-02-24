b2c1dde357c213bc77af6f9927840f50f979cfaf
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-24 17:36:17 +0000

    update demo js website 3, page1

--- week5/demo-js-website-3/js/page1.js
+++ week5/demo-js-website-3/js/page1.js
@@ -5,4 +5,10 @@ function page_load(){
     console.log(msgText.toUpperCase())
 }
 
+function lnkArrayDemo1(){
+
+    const msgText = "# page1: lnkArrayDemo1"
+
+    console.log(msgText.toUpperCase())
+}
 
