824e4c06178c77c3d79000a4230763d147222697
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-25 21:00:20 +0000

    update demo js website 3, page2

--- week5/demo-js-website-3/js/page2.js
+++ week5/demo-js-website-3/js/page2.js
@@ -12,5 +12,34 @@ function lnkJsonDemo1()
     const msgText = "# page2: lnkJsonDemo1"
 
     console.log(msgText.toUpperCase())
+
+    //json object variable 
+    const user = {"id":1,"name":"ann","email":"ann@gmail.com"}
+    
+    //verify root or starting element can be a [] or {}
+
+    //display json data as string 
+    console.log(JSON.stringify(user))
+
+    //retieve document field data 
+
+    //if (user == null | user.name == undefined)
+    console.log("name: ",user.name)
+
+    //verify invalid, valid document 
+
+    //retieve nested json document fields
+
+    //loop through json documents , nested loops 
+    
+    //verify element level or starting element can be a [] or {}
+
+    //retiieve nested json document fields or array fields reference
+    
+    //verify element level or starting element can be a [] or {}
+
+    //convert json string to json object 
+
 }
+
 
