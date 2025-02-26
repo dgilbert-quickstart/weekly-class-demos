6ed6c127ff8d3f6b0f0bb90c3f2024a7b8021a92
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-26 17:48:56 +0000

    update js demo website 3, page2

---
+++ week5/demo-js-website-3/js/page3.js
@@ -0,0 +1,35 @@
+function page_load(){
+
+    const msgText = "# page3: page load"
+
+    console.log(msgText.toUpperCase())
+}
+
+
+function lnkJsonApiDemo1()
+{
+    
+    const msgText = "# page3: lnkJsonApiDemo1"
+
+    console.log(msgText.toUpperCase())
+
+    //retrieve Api data 
+
+    //verify root or starting element can be a [] or {}
+
+    //verify invalid, valid document 
+
+    //retieve nested json document fields
+
+    //loop through json documents , nested loops 
+    
+    //verify element level or starting element can be a [] or {}
+
+    //retiieve nested json document fields or array fields reference
+    
+    //verify element level or starting element can be a [] or {}
+
+    //convert json string to json object 
+
+}
+
