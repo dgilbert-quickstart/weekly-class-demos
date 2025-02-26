bb244663acf079a4483c8bf99819c71bf331b2b3
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-26 16:56:00 +0000

    update js demo website 3, page2

--- week5/demo-js-website-3/js/page2.js
+++ week5/demo-js-website-3/js/page2.js
@@ -63,6 +63,48 @@ function lnkJsonDemo2()
     
     //example json document 1 {} , example json document 2 - [{},{}] , other nested document examples
 
+    //json object array variable 
+    const users1 = [{"id":1,"name":"ann","email":"ann@gmail.com"}
+        ,{"id":2,"name":"sam","email":"sam@gmail.com"}] //json object array 
+    
+    //json document can be a string, convert string to json object 
+    const str_users2 = `[{"id":1,"name":"ann","email":"ann@gmail.com"},
+    {"id":2,"name":"sam","email":"sam@gmail.com"}]` //string 
+    
+    const users2 = JSON.parse(str_users2); //convert json string to json object ie: api result 
+
+    //verify root or starting element can be a [] or {}
+
+    //display json data as string 
+    console.log("-- users1 ---")
+    console.log(JSON.stringify(users1))
+
+    console.log("-- users2 ---")
+    console.log(str_users2)  //string 
+    console.log(JSON.stringify(users2)) //json object to string
+
+    //retieve document field data 
+
+    //if (user == null | user.name == undefined)
+    console.log("users1[0].name: ",users1[0].name)
+    console.log("users2[1].name: ",users2[1].name)
+
+    //#nested documents field access 
+    //console.log(user.address.street)
+    
+    //#nested documents array field access 
+    //console.log(user.emails[0].email)
+
+    console.log("")
+    console.log(`//# loop through json object array `);
+    
+    //# loop through json object array 
+    //or nested for loops
+    for(i=0;i<users1.length;i++)
+    {
+        console.log(`# name: ${users1[i].name}, email: ${users1[i].email}`);
+    }
+
     //verify root or starting element can be a [] or {}
 
     //display json data as string
