134ac8e8e1ae6c07324600532a1f284f19b7ca87
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-02-26 21:34:55 +0000

    update js demo website 3, page3

--- week5/demo-js-website-3/js/page3.js
+++ week5/demo-js-website-3/js/page3.js
@@ -70,3 +70,47 @@ function lnkJqueryJsonApiDemo1()
 
 }
 
+function lnkFetchJsonApiDemo1()
+{
+    
+    let msgText = "# page3: lnkFetchJsonApiDemo1"
+
+    console.log(msgText.toUpperCase())
+
+    const divdisplayinfo = document.getElementById("divdisplayinfo");
+
+    if (divdisplayinfo == null | divdisplayinfo == undefined){       
+        msgText = "## Page3 - divdisplayinfo not found"
+        console.log(msgText) 
+        return false;
+    }
+
+    divdisplayinfo.innerText = msgText;
+
+    const _api_url_endpoint_users = "https://jsonplaceholder.org/users";
+    const _api_url_endpoint_comments = "https://jsonplaceholder.org/comments";
+    const _api_url_endpoint_posts = "https://jsonplaceholder.org/posts";
+
+    //api key example url
+    //const _api_url_endpoint_users_apikey = "https://jsonplaceholder.org/users/apik=youra-apikey";
+
+    //Fetch Api 
+
+    //verify root or starting element can be a [] or {}
+
+    //verify invalid, valid document 
+
+    //retieve nested json document fields
+
+    //loop through json documents , nested loops 
+    
+    //verify element level or starting element can be a [] or {}
+
+    //retiieve nested json document fields or array fields reference
+    
+    //verify element level or starting element can be a [] or {}
+
+    //convert json string to json object 
+
+}
+
