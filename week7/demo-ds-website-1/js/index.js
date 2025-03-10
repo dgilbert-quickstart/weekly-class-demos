dd84a0ad613b819a5a5b4cbf3c3b508f6eae96d6
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-10 18:27:03 +0000

    update demo ds website 1, page1

--- week7/demo-ds-website-1/js/index.js
+++
@@ -1,8 +0,0 @@
-
-function page_load()
-{
-    const msgText = "#page1::page_load"
-
-    console.log(msgText.toUpperCase())
-}
-
---
+++ week7/demo-ds-website-1/js/page1.js
@@ -0,0 +1,17 @@
+
+function page_load()
+{
+    const msgText = "#page1::page_load"
+
+    console.log(msgText.toUpperCase())
+}
+
+
+function array_demo_1()
+{
+    let msgText = "#page1::array_demo_1"
+
+    console.log(msgText.toUpperCase())
+}
+
+
