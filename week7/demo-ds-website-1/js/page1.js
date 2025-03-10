bda8030aa3ddf4fcec67b28386f3bb52f6b0038e
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-10 18:33:57 +0000

    update demo ds website 1, page1

--- week7/demo-ds-website-1/js/page1.js
+++ week7/demo-ds-website-1/js/page1.js
@@ -10,8 +10,17 @@ function page_load()
 function array_demo_1()
 {
     let msgText = "#page1::array_demo_1"
+    const divdisplay = document.getElementById("divdisplay"); //HTMLElement or null
+
+    if(divdisplay == null | divdisplay == undefined){
+
+        msgText = "#page1::divdisplay not found"
+        console.log(msgText)
+        return false;
+    }
 
     console.log(msgText.toUpperCase())
+    divdisplay.innerText = msgText;
 }
 
 
