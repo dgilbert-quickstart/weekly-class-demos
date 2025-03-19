7de7c0fd2d07fd958b70631d92b17546885f9332
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-19 22:06:48 +0000

    update react demo app 1 - create-react-app, yarn, vite

--- week8/react-development/react-demo-app-1/src/App.js
+++ week8/react-development/react-demo-app-1/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+/*
+jsx - page/component
+ie: App = react component / page 
+export function - jsx/ component/page 
+- other components can re-use app component
+*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link" 
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /*
+      1 top level container elemen
+      can have multiple container element 
+      top level - div 
+      top <></>
+    */
+   <>
+    <div>
+        jsx 
     </div>
+     <div>
+      <h2>Demo react app v1</h2>
+    </div>
+    <p></p>
+    <a href="#">display info demo 1</a>
+    <p></p>
+    <div id="divdisplay">
+
+    </div>
+   </>
   );
 }
 
