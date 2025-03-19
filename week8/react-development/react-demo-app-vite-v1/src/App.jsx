23f8fbf0075304e7027f384ea8b00a35c629d34c
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-19 22:12:36 +0000

    update react demo app 1 - create-react-app, yarn, vite

--- week8/react-development/react-demo-app-vite-v1/src/App.jsx
+++ week8/react-development/react-demo-app-vite-v1/src/App.jsx
@@ -1,11 +1,32 @@
-
+/*
+jsx - page/component
+ie: App = react component / page 
+export function - jsx/ component/page 
+- other components can re-use app component
+*/
 function App() {
-
   return (
-      <div>
-          <h2>Demo react vite app 1.0</h2>
-      </div>
-  )
+    /*
+      1 top level container elemen
+      can have multiple container element 
+      top level - div 
+      top <></>
+    */
+   <>
+    <div>
+        jsx 
+    </div>
+     <div>
+      <h2>Demo react app v1</h2>
+    </div>
+    <p></p>
+    <a href="#">display info demo 1</a>
+    <p></p>
+    <div id="divdisplay">
+
+    </div>
+   </>
+  );
 }
 
-export default App
+export default App;
