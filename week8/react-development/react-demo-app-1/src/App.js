8c534e2589ecd1753f5dab8ba1fa157ec82186af
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-20 16:58:29 +0000

    update react demo app 1 - create-react-app, yarn, vite

--- week8/react-development/react-demo-app-1/src/App.js
+++ week8/react-development/react-demo-app-1/src/App.js
@@ -5,6 +5,21 @@ export function - jsx/ component/page
 - other components can re-use app component
 */
 function App() {
+
+  //-- function inside a function 
+  function lnkDisplayInfo1()
+  {
+    let msgText = "# App::lnkDisplayInfo1"
+    
+    //alert(msgText)
+    console.log(msgText)
+
+  }
+
+  //--- function defined before retrun (
+  //react eslint - javascrcipt compiler hints, errors, warnings
+  //Line 10:12:  'lnkDisplayInfo1' is defined but never used
+
   return (
     /*
       1 top level container elemen
@@ -20,7 +35,7 @@ function App() {
       <h2>Demo react app v1</h2>
     </div>
     <p></p>
-    <a href="#">display info demo 1</a>
+    <a href="#" onClick={lnkDisplayInfo1}>display info demo 1</a>
     <p></p>
     <button>display info demo 2</button>
     <p></p>
