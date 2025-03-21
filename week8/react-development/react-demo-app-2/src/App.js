07b23c07e1ee4c487f35f203b7007ee8efcdb932
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-21 17:17:59 +0000

    update react demo app 2 - create-react-app, yarn, vite

--- week8/react-development/react-demo-app-2/src/App.js
+++ week8/react-development/react-demo-app-2/src/App.js
@@ -2,23 +2,42 @@
 function App() {
 
   //functions 
+  function lnkdemo1()
+  {
+    let msgText = "# App:: lnkdemo1"
+    console.log(msgText)
+  }
+
+  //es5+ arrow functions - optimized functions 
+  const lnkdemo2 = () =>
+  {
+    let msgText = "# App:: lnkdemo1"
+    console.log(msgText)
+  }
+
+  const btndemo1 = () =>
+  {
+    let msgText = "# App:: btndemo1"
+    console.log(msgText)
+  }
+
   //components - jsx - function retun(<> ..jsx.. </>)
 
   return (
     <>
-    <div>
-        <h1>React Demo app v02</h1>
-        <p></p>
-        <a href="#">link demo 1</a>
-        <p></p>
-        <a href="#">link demo 1</a>
-        <p></p>
-        <button>button demo 1</button>
-        <p></p>
-        <p>
-          Footer @ 2025
-        </p>
-    </div>
+      <div>
+          <h1>React Demo app v02</h1>
+          <p></p>
+          <a href="#" onClick={lnkdemo1}>link demo 1</a>
+          <p></p>
+          <a href="#" onClick={lnkdemo2}>link demo 1</a>
+          <p></p>
+          <button onClick={btndemo1}>button demo 1</button>
+          <p></p>
+          <p>
+            Footer @ 2025
+          </p>
+      </div>
     </>
   );
 }
