9c154833af758523915c19cd8adadc54526907d3
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-21 21:33:00 +0000

    update react demo app 2 - create-react-app, yarn, vite

--- week8/react-development/react-demo-app-2/src/App.js
+++ week8/react-development/react-demo-app-2/src/App.js
@@ -1,9 +1,17 @@
-import { useRef } from "react" 
+import { useRef, useState } from "react" 
 
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
 function App() {
+
+  //-- useState = create in-memory data accessable by all functions/components on the current component/page 
+  // imported components does not have access to useState 
+  
+  // useState [state-data-variable, function-name-to-update-state-date]
+  // es5+ [destructoring] = create array variables from a funtion 
+  // when usestate data is updated, UI auto updates data on scvreen called 2-way binding
+  const [strMsg, setStrMsg] = useState("")
 
   const divdisplay = useRef("") //refence, select manipluate jsx element ie: DOM
   
@@ -19,7 +27,12 @@ function App() {
     //react will create/generate the DOM document element code 
 
     divdisplay.current.innerText = msgText
+    
+    //update state varible using update state funciton
+    setStrMsg(msgText)
 
+    //-- error - can not update useState variable directly 
+    //strMsg = msgText
   }
 
   //es5+ arrow functions - optimized functions 
@@ -29,6 +42,12 @@ function App() {
     console.log(msgText)
     divdisplay.current.innerText = msgText
 
+    //update state varible using update state funciton
+    setStrMsg(msgText)
+
+    //-- error - can not update useState variable directly 
+    //strMsg = msgText
+    
   }
 
   const btndemo1 = () =>
@@ -37,12 +56,20 @@ function App() {
     console.log(msgText)
     divdisplay.current.innerText = msgText
 
+    //update state varible using update state funciton
+    setStrMsg(msgText)
+
+    //-- error - can not update useState variable directly 
+    //strMsg = msgText
+    
   }
 
   return (
     <>
       <div>
           <Header title="React Demo app v02.0.2"/>
+          <p></p>
+          {strMsg}
           <p></p>
           <a href="#" onClick={lnkdemo1}>link demo 1</a>
           <p></p>
