7ed18145acaccd4a4f16fa05c992f59b823a6d8b
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-03-24 22:47:53 +0000

    update react demo app 2 - create-react-app, yarn, vite

---
+++ week9/react-dev-repo-copy/bak-react-demo-app-2/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <>
+          <p>Demo react app v2.0</p>
+          <div>
+            <a href="#">useRef demo 1</a><p></p>
+            <a href="#">useState demo 1</a><p></p>
+          </div>
+          <div>
+            [displa info]
+          </div>
+          <p>Footer @ 2025</p>
+    </>
+  );
+}
+
+export default App;
